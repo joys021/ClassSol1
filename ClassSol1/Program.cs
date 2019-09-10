26846bf5aada0c324c22e992935319f528e7e1b9
joy021 <joys021@gmail.com>
2019-09-10 19:37:30 -0400

    Add project files.

---
+++ ClassSol1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassSol1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter the years of experience you have in professional programming:");
+            year = Convert.ToInt32(Console.ReadLine());
+            switch (year)
+            {
+                case (0):
+                    Console.WriteLine("Need a lots of practise");
+                    break;
+                case (1):
+                    Console.WriteLine("Looks like you have a little experience");
+                    break;
+                default:
+                    Console.WriteLine("You are an expert");
+                    break;
+            }
+        }
+    }
+}
